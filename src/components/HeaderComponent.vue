48ee2f5f87f0293f8816efe52ba62794dbbe6621
Kim Robert <kimrobert.nb@gmail.com>
2023-07-15 00:06:31 +0200

    i.1.3 feat: wip app global layout and add icons

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,7 +9,47 @@
                 <h1 class="header-title">DEvMO</h1>
                 <p class="header-subtitle">by kimro</p>
             </div>
-            <NavbarComponent /> 
+            <NavbarComponent class="navbar-component" /> 
         </div>
     </header>
 </template>
+
+<style lang="scss">
+    @import '@/styles/theme.scss';
+
+    .header {
+        box-sizing: border-box;
+        max-height: 140px;
+        padding: $space-s;
+        border-bottom: 4px double $primary;
+    }
+
+    .header-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    .header-title-container {
+        display: flex;
+        flex: 3;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: $primary;
+        .header-title {
+            line-height: 1.5;
+            margin: 0;
+            font-weight: 800;
+        }
+        .header-subtitle {
+            line-height: 1;
+            margin: 0;
+        }
+    }
+
+    .navbar-component {
+        flex: 1;
+    }
+</style>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
+    import { Bars3Icon, EnvelopeIcon } from '@heroicons/vue/24/outline';
 
     const showNav = ref<boolean>(false);
     const toggleNav = (): void => {
@@ -11,12 +12,61 @@
     <div class="nav-container">
         <nav class="navbar">
             <div class="navmenu-container">
-                <button type="button" class="nav-button __toggle" @click="toggleNav">*</button>
+                <button type="button" class="nav-button __toggle" @click="toggleNav"><Bars3Icon class="nav-icon __toggle" /></button>
                 <ol class="navmenu" v-if="showNav">
                     <li class="navitem">Menu de navigation</li>
                 </ol>
             </div>
-            <RouterLink to="/contact" class="nav-button __contact">¤</RouterLink>
+            <RouterLink to="/contact" class="nav-button __contact">
+                <EnvelopeIcon class="nav-icon __user" />
+            <BeakerIcon class="nav-icon" /></RouterLink>
         </nav>
     </div>
 </template>
+
+<style lang="scss">
+    @import '@/styles/theme.scss';
+
+    .navbar {
+        max-width: 240px;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .navmenu-container {
+        max-width: 120px;
+        position: relative;
+    }
+
+    .nav-button {
+        width: 60px;
+        height: 60px;
+        margin: $space-xs;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        background-color: transparent;
+        color: $primary;
+        border-radius: $radius-s;
+        border: 1px solid $primary;
+        padding: $space-s;
+        transition: all 200ms ease-in;
+
+        .nav-icon {
+            cursor: pointer;
+            height: 1.5rem;
+            width: auto;
+            color: $primary;
+        }
+
+        &:hover, &:focus {
+            background-color: $primary;
+            .nav-icon {
+                color: $light;
+            }
+        }
+    }
+    
+</style>
