73b795538b03551967d813f50cfd4bf0f620ac37
Kim Robert <kimrobert.nb@gmail.com>
2023-07-16 15:17:07 +0200

    f.1.1 feat: add nantes map

--- src/components/homepage/AimsComponent.vue
+++ src/components/homepage/AimsComponent.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-    import { EyeIcon, BriefcaseIcon, AcademicCapIcon } from '@heroicons/vue/24/outline';
+    import { EyeIcon, BriefcaseIcon, AcademicCapIcon, MapPinIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
@@ -10,12 +10,20 @@
                 <SectionTitleParticle title="What am I looking for ?" />
             </div>
             <div class="aims-container">
-                <h3 class="title-ter">Wanted Jobs:</h3>
-                <p class="aims-subtitle">"En alternance"</p>
-                <ol class="aims-list">
-                    <li class="aim-item"><BriefcaseIcon class="icon-classic" />Full-stack web developer</li>
-                    <li class="aim-item"><BriefcaseIcon class="icon-classic" />Front-end web developer</li>
-                </ol>
+                <div class="aims-bloc">
+                    <div class="aims-subbloc">
+                        <h3 class="title-ter">Wanted Jobs:</h3>
+                        <p class="aims-subtitle">"En alternance"</p>
+                        <ol class="aims-list">
+                            <li class="aim-item"><BriefcaseIcon class="icon-classic" />Full-stack web</li>
+                            <li class="aim-item"><BriefcaseIcon class="icon-classic" />Front-end web</li>
+                            <li class="aim-item"><MapPinIcon class="icon-classic" />In (or near) Nantes</li>
+                        </ol>
+                    </div>
+                    <div class="aims-img-container">
+                        <img src="@images/nantes_map.webp" alt="a map showing nantes city" class="aims-img" />
+                    </div>
+                </div>
             </div>
             <div class="aims-container">
                 <h4 class="title-fourth">Aimed Degree:</h4>
@@ -39,6 +47,38 @@
         border-top: 1px solid $primary;
     }
 
+    .aims-bloc {
+        width: 100%;
+        display: flex;
+        flex-direction:column-reverse;
+        @media (min-width: $bp-m) {
+            flex-direction: row;
+        }
+        .aims-subbloc {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            @media (min-width: $bp-m) {
+                width: 50%;
+            }
+        }
+    }
+
+    .aims-img-container {
+        display: flex;
+        justify-content: center;
+        @media (min-width: $bp-m) {
+            width: 50%;
+            justify-content: flex-start;
+        }
+        .aims-img {
+            width: 100%;
+            max-width: 240px;
+            max-height: 350px;
+            margin: $space-s 0;
+        }
+    }
+
     .aims-subtitle {
         font-size: $txt-xs;
         font-weight: 200;
@@ -50,6 +90,9 @@
     .aims-list {
         list-style-type: none;
         padding: 0;
+        @media (min-width: $bp-m) {
+            width: 50%;
+        }
         .aim-item {
             font-size: $txt-s;
             font-weight: 400;
