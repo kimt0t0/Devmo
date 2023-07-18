1a0853a0f3e00adf203dcca6093084f4842cad00
Kim Robert <kimrobert.nb@gmail.com>
2023-07-18 13:18:09 +0200

    f.3.1 feat: wip training item component and training list

---
+++ src/components/training/TrainingItemComponent.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+    import { BriefcaseIcon, AcademicCapIcon } from '@heroicons/vue/24/outline';
+    defineProps<{
+        title: string;
+        subtitle: string;
+        color?: string;
+        icon?: string;
+    }>();
+</script>
+
+<template>
+    <li :class="'training-item __' + color">
+        <p :class="'ti-title __' + color">
+            <AcademicCapIcon class="ti-icon" v-if="icon === 'degree' || !icon" />
+            <BriefcaseIcon class="ti-icon" v-if="icon === 'job'" />
+            <strong>{{ title }}</strong>
+        </p>
+        <p :class="'ti-subtitle __' + color">{{ subtitle }}</p>
+    </li>
+</template>
+
+<style lang="scss">
+    @import '@/styles/theme.scss';
+
+    .training-item {
+        margin: $space-m 0;
+
+        .ti-title {
+            display: flex;
+            align-items: center;
+            font-size: $txt-s;
+            font-weight: 600;
+            margin: 0;
+            line-height: 1.5;
+
+            .ti-icon {
+                width: $txt-s;
+                height: $txt-s;
+                margin-right: $space-s;
+            }
+        }
+
+        .ti-subtitle {
+            font-size: $txt-xs;
+            font-style: oblique;
+            font-weight: 200;
+            margin: 0;
+        }
+    }
+</style>
--- src/views/TrainingView.vue
+++ src/views/TrainingView.vue
@@ -1,6 +1,41 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    import TrainingItemComponent from '@/components/training/TrainingItemComponent.vue';
+</script>
 
 <template>
     <HeroParticle title="My Training, Degrees and Experience" subtitle="If you want to knwo more about my studies and previous experience, this is the right place." />
     <!-- add illustration here -->
+    <section class="classic-section">
+        <div class="classic-container">
+            <ol class="training-list">
+                <TrainingItemComponent 
+                title="Titre Concepteur Développeur d'Applications"
+                subtitle="2022-2023, MyDigitalSchool Nantes" />
+        
+                <TrainingItemComponent
+                title="TOEIC 20/20"
+                subtitle="2022-2023, MyDigitalSchool Nantes" />
+
+                <TrainingItemComponent
+                title="ANSSI Cybersecurity MOOC"
+                subtitle="2022-2023, MyDigitalSchool Nantes" />
+
+                <TrainingItemComponent
+                title="Front-End Web Developer"
+                subtitle="2022-2023 Studio Floax (Vertou)" />
+            </ol>
+        </div>
+    </section>
 </template>
+
+<style lang="scss">
+    @import '@/styles/theme.scss';
+
+    .training-list {
+        list-style-type: none;
+        background-color: $light;
+        border: 1px solid transparent;
+        border-radius: $radius-xs;
+        padding: $space-s $space-xl;
+    }
+</style>
