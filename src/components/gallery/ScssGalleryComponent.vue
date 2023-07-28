3ff0492563deb876df469b43ca8bf6eb5ed12b03
Kim Robert <kimrobert.nb@gmail.com>
2023-07-28 12:14:35 +0200

    f.5.1 feat: wip gallery add scss gallery component and first css piece

---
+++ src/components/gallery/ScssGalleryComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+    import SunCircle from './scss/SunCircleComponent.vue';
+</script>
+
+<template>
+    <section class="classic-section scss-gallery-section">
+        <h3 class="title-ter scss-gallery-title">Styling Gallery</h3>
+        <SunCircle />
+    </section>
+</template>
+
+<style lang="scss">
+    @import '@/styles/theme.scss';
+
+    .scss-gallery-title {
+        margin-bottom: $space-l;
+    }
+</style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+    import ScssGallery from '@/components/gallery/ScssGalleryComponent.vue';
     const anchors = [
         {
             path: '#css-scss',
@@ -6,19 +7,19 @@
         },
         {
             path: '#vue-at-work',
-            title: 'Vue at work'
+            title: 'Vue (work experience)'
         },
         {
             path: '#nest-js',
-            title: 'Nest.js & TypeOrm at work'
+            title: 'Nest.js & TypeOrm (work experience)'
         },
         {
             path: '#basic-security',
             title: 'Basic security (Node & Express & Mongo)'
         },
         {
-            path: '#online',
-            title: 'Full front online examples from school'
+            path: '#basic-tdd',
+            title: 'Basic Test Driven Development'
         }
     ]
 </script>
@@ -26,4 +27,7 @@
 <template>
     <HeroParticle title="Check my gallery !" />
     <IndexParticle :anchors="anchors" />
+    <div class="classic-container gallery-container">
+        <ScssGallery />
+    </div>
 </template>
