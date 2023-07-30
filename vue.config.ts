8bb3b7f094be8b3355da2eb1a0a9ef9e1dc7669a
Kim Robert <kimrobert.nb@gmail.com>
2023-07-30 22:10:14 +0200

    adding dist subtree

--- vue.config.ts
+++ vue.config.ts
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/devmo/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/devmo/" : "/",
+  transpileDependencies: true
 })
