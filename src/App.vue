9ad4db22ca888a70e25e50e6a5179af790dfd514
Kim Robert <kimrobert.nb@gmail.com>
2023-10-11 10:28:19 +0200

    i.3.5 feat: add transition when page contents change

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,34 @@ import LegalsComponent from './components/footer/LegalsComponent.vue';
   <LegalsComponent v-if="useLegalsStore().showLegals" />
   <main class="main">
     <LanguageMenuComponent />
-    <RouterView />
+    <Transition name="slide-fade">
+      <RouterView />
+    </Transition>
   </main>
   <FooterComponent />
 </template>
 
-<style>
+<style lang="scss">
 @import '@/styles/theme.scss';
 
 .main {
   min-height: 800px;
   padding: 140px 0 80px;
   position: relative;
+}
+
+/* Transition effect */
+.slide-fade-enter-active {
+  transition: all 0.4s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
